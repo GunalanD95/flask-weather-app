788ab8a9ff8d0f5dc0c08c4eee08606a9aa3c857
GunalanD95 <guna19may2015@gmail.com>
2022-02-11 20:04:36 +0530

    change

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request
 API_KEY = 'f486f5938741e8cf9f1cd71cdc0d9d37'
 
 
-app = Flask()
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     """
     Returns the index page
     """
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 def get_weather_res(request,city_name):
@@ -18,3 +18,7 @@ def get_weather_res(request,city_name):
     print("url", url)
     get_req = requests.get(url)
     return get_req.json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
